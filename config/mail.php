f9f5b1c84d646b971925c5e0f041cffc1d3831e1
mulki12 <miftahul.mulki95@gmail.com>
2021-11-22 08:32:51 +0000

    second commit

--- config/mail.php
+++ config/mail.php
@@ -38,6 +38,10 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
+            'from' => [
+                'address' => 'cvmiladiyyah@gmail.com', 
+                'name' => 'otentikasi'
+            ],
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
